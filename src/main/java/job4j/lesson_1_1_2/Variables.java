2a59450df9007019966c8e9db7da468940be0db6
Kingmen77 <viktor-vasilev@inbox.ru>
2021-06-15 22:26:22 +0300

    Hello

---
+++ src/main/java/job4j/lesson_1_1_2/Variables.java
@@ -0,0 +1,9 @@
+package job4j.lesson_1_1_2;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+    }
+}
