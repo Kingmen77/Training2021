33adf894461dec4c8902ad5ca0a5bd1a7939d8d4
Kingmen77 <viktor-vasilev@inbox.ru>
2021-06-16 22:19:13 +0300

    Переназначение переменной Задание 1. В комментарии к этой задаче напишите код.
    В коде нужно объявить переменную size типа int и присвоить ей значение 10.
    
    Далее нужно назначить этой же переменной значение 100.
    
    Потом нужно отнять от переменной size число 5, и результат записать опять в переменную size.
    
    В вашем коде будет создана только одна переменная size.

---
+++ src/main/java/job4j/lesson_1_1_2/Arithmetic.java
@@ -0,0 +1,15 @@
+package job4j.lesson_1_1_2;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePiusTwo = one + two;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+    }
+}
