e2137bb0af78885c89320aced291782b9ae53776
Kingmen77 <viktor-vasilev@inbox.ru>
2021-07-04 00:17:40 +0300

    4.1. Аргументы. Исправленный

--- src/main/java/job4j/lesson_1_1_2/ArgMethod.java
+++ src/main/java/job4j/lesson_1_1_2/ArgMethod.java
@@ -1,17 +1,18 @@
 package job4j.lesson_1_1_2;
 
 public class ArgMethod {
-    public static void hello(int age, String name) {
-        System.out.println("Hello, " + name + ", age = " + age);
+    public static void hello(String name, Integer age) {
+        String actualName = name != null ? name : "Mr Smith";
+        String ageMessage = age != null ? ", age = " + age : ".";
+        System.out.println("Hello, " + actualName + ageMessage);
     }
-    public static void hello(String name, int age) {
 
-    }
     public static void hello(String name) {
-
+        ArgMethod.hello(name, null);
     }
-    public static void hello(int age) {
 
+    public static void hello(Integer age) {
+        ArgMethod.hello(null, age);
     }
 
     public static void main(String[] args) {
@@ -20,9 +21,6 @@ public class ArgMethod {
         ArgMethod.hello(name);
         ArgMethod.hello(name, age);
         ArgMethod.hello(age);
-        ArgMethod.hello(age, name);
-
+        ArgMethod.hello(name, age);
     }
-
-
 }
